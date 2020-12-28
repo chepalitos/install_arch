6a34bfc179092ae1ec4feb2068d6fcdff8d5351f
PalitosChinos <chepalitos@gmail.com>
2020-12-28 20:47:23 -0300

    Create instalarch.sh

---
+++ instalarch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo -n "Password: "
+read -s password
+echo
+echo -n "Repeat Password: "
+read -s password2
+echo
+[[ "$password" == "$password2" ]] || ( echo "Passwords did not match"; exit 1; )
