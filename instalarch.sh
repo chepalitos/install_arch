aaa20ec70c2fbcfc77b03647e8aea906bc6744b2
PalitosChinos <chepalitos@gmail.com>
2020-12-28 20:57:30 -0300

    Update instalarch.sh

--- instalarch.sh
+++ instalarch.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# sudo pacman -Sy
+# sudo pacman -S git
+# chmod +x instalarch.sh
+# git clone https://github.com/palitoschinos/instalarch
 
 echo -n "Ingrese el numero de la unidad donde se montara /mnt: "
 read -s mnt_number
 echo -n "Vamos a formatear la unidad /dev/sda$mnt_number"
-echo -n "Repeat Password: "
-read -s password2
-echo
+echo -n
+
 #[[ "$password" == "$password2" ]] || ( echo "Passwords did not match"; exit 1; )
