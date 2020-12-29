cb92ce7ecd92cb647c72efd3d39d078744edaff3
PalitosChinos <chepalitos@gmail.com>
2020-12-29 16:43:10 -0300

    Update and rename inalarplasma to inalarplasma.sh

--- inalarplasma.sh
+++ inalarplasma.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-# Fully update your Arch system:
-# pacman -Syu  # If a new kernel becomes available and is now installed, reboot, before proceeding.
+# pacman -Sy
+# pacman -S git
+# cd /tmp
+# git clone https://github.com/palitoschinos/instalarch
+# cd instalarch
+# chmod +x instalarch.sh
+# ./instalarch.sh
 
 # If you don't have network connectivity in your Arch system, run:
 # systemctl start dhcpcd <ethernet or wlan interface name>
+# systemctl enable dhcpcd
+# ip link set <ethernet or wlan interface name> up
 
+# Fully update your Arch system:
+# pacman -Syu  # If a new kernel becomes available and is now installed, reboot, before proceeding.
 # Now that you have an updated system, do:
 # pacman -S linux-headers
 # pacman -S dkms  # This will automatically rebuild your kernel modules as new upstream kernels are released.
-
 
 pacman -S xorg xorg-xinit
 pacman -S xorg-drivers # For all if you do not know
