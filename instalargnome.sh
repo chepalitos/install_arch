9c4d3f2b54dab25b6a459d2c3b5db51b87326e16
PalitosChinos <chepalitos@gmail.com>
2020-12-29 12:01:06 -0300

    Update and rename instalarge.sh to instalargnome.sh

---
+++ instalargnome.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Fully update your Arch system:
+# pacman -Syu  # If a new kernel becomes available and is now installed, reboot, before proceeding.
+
+# If you don't have network connectivity in your Arch system, run:
+# systemctl start dhcpcd <ethernet or wlan interface name>
+
+# Now that you have an updated system, do:
+# pacman -S linux-headers
+# pacman -S dkms  # This will automatically rebuild your kernel modules as new upstream kernels are released.
+
+pacman -S xorg xorg-xinit
+pacman -S xorg-drivers # For all if you do not know
+pacman -S xorg-apps
+
+pacman -S gnome-shell nautilus gnome-terminal gnome-tweak-tool gnome-control-center xdg-user-dirs  gnome-keyring
+
+# nano /etc/locale.gen # uncomment the needed lines like: es_Es.UTF-8 or es_AR.UTF-8
+# locale-gen
+
+pacman -S networkmanager
+
+systemctl stop dhcpcd
+systemctl disable dhcpcd
+
+sudo systemctl enable NetworkManager
+sudo systemctl start NetworkManager
+
+pacman -S gdm
+systemctl enable gdm
+systemctl start gdm
+
+echo "are we still alive?"
