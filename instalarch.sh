2b70cafdcf7df48b3121534021b982834e21813a
PalitosChinos <chepalitos@gmail.com>
2020-12-28 21:45:09 -0300

    Update instalarch.sh

--- instalarch.sh
+++ instalarch.sh
@@ -11,17 +11,17 @@ timedatectl set-ntp true
 echo -n "Ingrese el numero de la unidad donde se montara /swap: "
 read -s swap_number
 echo
-echo -n "Vamos a montar swapn en la unidad /dev/sda{$mnt_number}"
-mkswap /dev/sda${swap_number}
-swapon /dev/sda${swap_number}
+echo -n "Vamos a montar swapn en la unidad /dev/sda$mnt_number"
+mkswap /dev/sda$swap_number
+swapon /dev/sda$swap_number
 
 echo -n "Ingrese el numero de la unidad donde se montara /mnt: "
 read -s mnt_number
 echo
-echo -n "Vamos a formatear la unidad /dev/sda{$mnt_number}"
+echo -n "Vamos a formatear la unidad /dev/sda$mnt_number"
 
-mkfs.ext4 /dev/sda{$mnt_number}
-mount /dev/sda{$mnt_number} /mnt
+mkfs.ext4 /dev/sda$mnt_number
+mount /dev/sda$mnt_number /mnt
 
 mkdir /mnt/boot
 mount /dev/sda2 /mnt/boot
@@ -45,14 +45,14 @@ hwclock --show
 echo -n "Ingrese el nombre del host: "
 read -s host_name
 echo
-echo ${host_name} > /etc/hostname
+echo $host_name > /etc/hostname
 passwd
 
 echo -n "Ingrese el nombre del usuario: "
 read -s usr_name
 echo
-useradd -m -G wheel,storage,power -s /bin/bash ${usr_name}
-passwd ${usr_name}
+useradd -m -G wheel,storage,power -s /bin/bash $usr_name
+passwd $usr_name
 
 echo "LANG=es_AR.UTF-8" > /etc/locale.gen
 echo "LANG=es_AR.UTF-8" > /etc/locale.conf
@@ -61,7 +61,7 @@ echo "KEYMAP=es" > /etc/vconsole.conf
 echo ${host_name} > /etc/hostname
 echo -e "127.0.0.1\tlocalhost" > /etc/hosts
 echo -e "::1\t\tlocalhost" >> /etc/hosts
-echo -e "127.0.1.1\t${host_name}.${usr_name} ${host_name}" >> /etc/hosts
+echo -e "127.0.1.1\t$host_name.$usr_name $host_name" >> /etc/hosts
 cat /etc/hosts
 
 locale-gen
@@ -71,7 +71,7 @@ mkinitcpio -p linux
 echo -n "Ingrese el id de la instalacion en para el bootloader: "
 read -s id_name
 echo
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=4{id_name}
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=$id_name
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Exit Your New Arch System
