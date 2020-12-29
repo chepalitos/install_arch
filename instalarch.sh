5a562c133764cfd5503d727d884f07a4fae28f76
PalitosChinos <chepalitos@gmail.com>
2020-12-28 21:53:37 -0300

    Update instalarch.sh

--- instalarch.sh
+++ instalarch.sh
@@ -31,13 +31,15 @@ mount /dev/sda1 /mnt/boot/efi
 
 pacstrap /mnt base base-devel grub efibootmgr dialog wpa_supplicant linux linux-headers nano dhcpcd iwd lvm2 linux-firmware --noconfirm
 
+echo "genfstab"
 genfstab -U /mnt >> /mnt/etc/fstab
-cat /mnt/etc/fstab
 
 arch-chroot /mnt /bin/bash
 
+echo "systemctl enable iwd"
 systemctl enable iwd
 
+echo "set hwclock"
 ln -s /usr/share/zoneinfo/Argentina/Buenos_Aires /etc/localtime
 hwclock --systohc --utc
 hwclock --show
@@ -64,14 +66,18 @@ echo -e "::1\t\tlocalhost" >> /etc/hosts
 echo -e "127.0.1.1\t$host_name.$usr_name $host_name" >> /etc/hosts
 cat /etc/hosts
 
+echo "locale-gen"
 locale-gen
 
+
+echo "mkinitcpio"
 mkinitcpio -p linux
 
 echo -n "Ingrese el id de la instalacion en para el bootloader: "
 read -s id_name
-echo
+echo "grub-install"
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=$id_name
+echo "grub-mkconfig"
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Exit Your New Arch System
