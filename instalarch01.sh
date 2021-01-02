d4ed969bc111e419d9fb04166540b24f2fd603a6
PalitosChinos <chepalitos@gmail.com>
2021-01-01 23:32:39 -0300

    Update instalarch01.sh

--- instalarch01.sh
+++ instalarch01.sh
@@ -10,7 +10,10 @@
 # ls /sys/firmware/efi # To enshure it is it UEFI
 # lsblk
 
+# if U need to format /efi partition
 # mkfs.vfat -F 32 /dev/sdXX
+
+# if U need to format /boot partition
 # mkfs.ext4 /dev/sdXY
 
 timedatectl set-ntp true
@@ -28,11 +31,17 @@ echo $mnt_number
 mkfs.ext4 /dev/sda$mnt_number
 mount /dev/sda$mnt_number /mnt
 
+echo -n "Ingrese el numero de la unidad donde se montara /boot: "
+read -s boot_number
+echo $boot_number
 mkdir /mnt/boot
-mount /dev/sda2 /mnt/boot
+mount /dev/sda$boot_number /mnt/boot
 
+echo -n "Ingrese el numero de la unidad donde se montara /efi: "
+read -s efi_number
+echo $efi_number
 mkdir /mnt/boot/efi
-mount /dev/sda1 /mnt/boot/efi
+mount /dev/sda$efi_number /mnt/boot/efi
 
 echo -n "Ingrese el numero de la unidad donde se montara /home: "
 read -s home_number
@@ -43,9 +52,10 @@ mount /dev/sda$home_number /mnt/home
 # Check where is home mounted
 # findmt -n -o SOURCE --target /home
 
-pacstrap /mnt base linux linux-firmware efibootmgr grub nano dhcpcd iwd 
+pacstrap /mnt base linux linux-firmware efibootmgr grub nano dhcpcd iwd lvm2 net-tools man-pages
 # pacstrap /mnt base base-devel net-tools
-# pacstrap /mnt base base-devel grub efibootmgr dialog wpa_supplicant linux linux-headers nano dhcpcd iwd lvm2 linux-firmware net-tools
+# pacstrap /mnt base base-devel grub efibootmgr linux linux-headers nano dhcpcd iwd lvm2 linux-firmware net-tools
+# pacstrap net-tools, dnsutils e iputils dialog wpa_supplicant
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
