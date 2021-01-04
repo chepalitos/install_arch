de04dfd94a5df0011c3d502bb14f24a610ab8b1c
PalitosChinos <chepalitos@gmail.com>
2021-01-04 18:15:34 -0300

    Update instalarch02.sh

--- instalarch02.sh
+++ instalarch02.sh
@@ -46,8 +46,8 @@ echo $id_name
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=$id_name
 
-#pacman -S os-prober
-#os-prober
+pacman -S os-prober
+os-prober
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable dhcpcd
