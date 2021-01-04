54abedd8bc8e0f052d1df1d802e0ee4c715a2868
PalitosChinos <chepalitos@gmail.com>
2021-01-04 12:33:07 -0300

    Update instalarch01.sh

--- instalarch01.sh
+++ instalarch01.sh
@@ -50,7 +50,7 @@ mount /dev/sda$home_number /mnt/home
 # Check where is home mounted
 # findmt -n -o SOURCE --target /home
 
-pacstrap /mnt base base-devel linux linux-firmware efibootmgr grub-efi-x86_64 nano man-pages iwd lvm2 net-tools man-pages
+pacstrap /mnt base base-devel linux linux-firmware efibootmgr grub-efi-x86_64 nano man-pages man-pages dhcpcd
 # pacstrap /mnt base linux linux-headers efibootmgr grub-efi-x86_64 nano dhcpcd iwd lvm2 net-tools man-pages dialog dnsutils iputils wpa_supplicant
 # pacstrap /mnt base base-devel net-tools
 # pacstrap /mnt base base-devel grub-efi-x86_64 efibootmgr linux linux-headers nano dhcpcd iwd lvm2 linux-firmware net-tools
