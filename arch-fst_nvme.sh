73e3568fddfeed60d2bd42104588ef5cf2789b7c
fschneeberger <fschneeberger@proton-arch>
2023-05-23 22:07:44 -0300

    proton

--- arch-fst_nvme.sh
+++ arch-fst_nvme.sh
@@ -6,40 +6,38 @@ lsblk
 echo -n ">>> Ingrese el numero de la unidad donde se montara /swap: "
 read -s swap_number
 echo $swap_number
-
-mkswap /dev/sda$swap_number
-swapon /dev/sda$swap_number
+mkswap /dev/nvme0n1p$swap_number
+swapon /dev/nvme0n1p$swap_number
 
 echo -n ">>> Ingrese el numero de la unidad donde se montara /root: "
 read -s mnt_number
 echo $mnt_number
 
-mkfs.ext4 /dev/sda$mnt_number
-mount /dev/sda$mnt_number /mnt
+mkfs.ext4 /dev/nvme0n1p$mnt_number
+mount /dev/nvme0n1p$mnt_number /mnt
 
 echo -n "Ingrese el numero de la unidad donde se montara /boot: "
 read -s boot_number
 echo $boot_number
 
-mkfs.ext4 /dev/sda$boot_number
+mkfs.fat -F32 /dev/nvme0n1p$boot_number
 mkdir /mnt/boot
-mount /dev/sda$boot_number /mnt/boot
+mount /dev/nvme0n1p$boot_number /mnt/boot
 
 echo -n ">>> Ingrese el numero de la unidad donde se montara /efi: "
 read -s efi_number
 echo $efi_number
 
-mkfs.fat -F32 /dev/sda$efi_number
+mkfs.fat -F32 /dev/nvme0n1p$efi_number
 mkdir /mnt/efi
-mount /dev/sda$efi_number /mnt/efi
-mount /dev/sda$efi_number /mnt/boot/efi
+mount dev/nvme0n1p$efi_number /mnt/efi
+mount dev/nvme0n1p$efi_number /mnt/boot/efi
 
 echo -n ">>> Ingrese el numero de la unidad donde se montara /home: "
 read -s home_number
 echo $home_number
-
 mkdir /mnt/home
-mount /dev/sda$home_number /mnt/home
+mount /dev/nvme0n1p$home_number /mnt/home
 
 # echo -n "Instalando archlinux-heyring\n"
 
@@ -49,9 +47,9 @@ mount /dev/sda$home_number /mnt/home
 
 echo -n ">>>> Instalando archlinux-heyring\n"
 # pacstrap /mnt base
-# pacstrap /mnt base linux-firmware linux
-pacstrap /mnt base linux linux-firmware efibootmgr grub-efi-x86_64 base-devel vim
-# linux-headers net-tools vim lvm2 nano iwd man-pages dialog dnsutils iputils wpa_supplicant dhcpcd
+pacstrap /mnt base linux-firmware linux
+#pacstrap /mnt base linux-firmware linux efibootmgr grub-efi-x86_64 base-devel vim
+#pacstrap /mnt base linux-firmware linux efibootmgr grub-efi-x86_64 base-devel vim linux-headers net-tools dnsutils iputils dhcpcd
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
