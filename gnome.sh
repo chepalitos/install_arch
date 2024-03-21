a5f6cf3f8e58c2946c35ec7dc5a020f8ea605eda
chepalitos <chepalitos@gmail.com>
2024-03-21 15:23:44 -0300

    proton

--- gnome.sh
+++ gnome.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install DKMS. This will automatically rebuild your kernel modules as new upstream kernels are released.
-pacman -S dkms
+sudo pacman -S dkms
 
 # pacman -S xorg-xwayland
 # pacman -S xf86-video-intel
@@ -9,20 +9,20 @@ pacman -S dkms
 # pacman -S xorg-server xorg-server-utils xorg-xinit xorg-apps xorg-drivers
 
 # pacman -S gnome
-pacman -S gnome-shell nautilus gnome-console gnome-system-monitor gnome-text-editor gnome-calculator gnome-screenshot gnome-control-center evince totem firefox tmux openssh networkmanager
+sudo pacman -S gnome-shell nautilus gnome-console gnome-system-monitor gnome-text-editor gnome-calculator gnome-screenshot gnome-control-center evince totem firefox tmux openssh networkmanager
 # pacman -S genome-shell-extensions epiphany gedit eye xdg-user-dirs network-manager-applet gnome-tweak-tool gnome-terminal
 # pacman -S gnome-keyring
 # pacman -S usbutils tcpdump
 
-systemctl enable NetworkManager
-systemctl start NetworkManager
+sudo systemctl enable NetworkManager
+sudo systemctl start NetworkManager
 
 #systemctl stop dhcpcd
 #systemctl disable dhcpcd
 
 # sudo pacman -S bluez bluez-utils
-systemctl enable bluetooth
-systemctl start bluetooth
+sudo systemctl enable bluetooth
+sudo systemctl start bluetooth
 
 # First off, I’ll install pulseaudio and the Bluetooth extension.
 #sudo pacman -S pulseaudio
@@ -33,9 +33,9 @@ systemctl start bluetooth
 # If you want it to start up automatically:
 #sudo systemctl start pulseaudio
 
-pacman -S gdm
-systemctl enable gdm
-systemctl start gdm
+sudo pacman -S gdm
+sudo systemctl enable gdm
+sudo systemctl start gdm
 
 echo "are we still alive?"
 
