3b3caf828593bb88dc57367a1448dd5a99eaa501
chepalitos <chepalitos@gmail.com>
2026-09-25 17:32:47 -0300

    proton

--- arch-fst-sdx-efi-boot.sh
+++ arch-fst-sdx-efi-boot.sh
@@ -48,11 +48,15 @@ mount /dev/sda$home_number /mnt/home
 # pacman -Sy archlinux-keyring
 
 echo -n ">>>> Instalando paquetes base\n"
-#pacstrap /mnt base
-#pacstrap /mnt base linux-firmware linux linux-headers base-devel
-pacstrap /mnt base linux-firmware linux linux-headers base-devel dhcpcd iputils dnsutils vim
-#pacstrap /mnt base linux-firmware linux efibootmgr grub-efi-x86_64 base-devel vim
-#pacstrap /mnt base linux-firmware linux efibootmgr grub-efi-x86_64 base-devel vim linux-headers net-tools dnsutils iputils dhcpcd
+
+# pacstrap /mnt base
+# pacstrap /mnt base linux-firmware linux
+
+# pacstrap /mnt base
+pacstrap /mnt base linux-firmware linux linux-headers base-devel
+# pacstrap /mnt base linux-firmware linux linux-headers base-devel dhcpcd iputils dnsutils vim
+# pacstrap /mnt base linux-firmware linux efibootmgr grub-efi-x86_64 base-devel vim
+# pacstrap /mnt base linux-firmware linux efibootmgr grub-efi-x86_64 base-devel vim linux-headers net-tools dnsutils iputils dhcpcd
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
