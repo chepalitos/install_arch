18d11a39864abc46102d0f8a7d2e8a89e741907d
chepalitos <chepalitos@gmail.com>
2026-09-24 23:15:07 -0300

    proton

--- arch-fst-sdx-efi-boot.sh
+++ arch-fst-sdx-efi-boot.sh
@@ -43,14 +43,16 @@ mount /dev/sda$home_number /mnt/home
 
 # Instalando archlinux-heyring
 
-pacman-key --init
-pacman-key --populate archlinux
-pacman -Sy archlinux-keyring
+# pacman-key --init
+# pacman-key --populate archlinux
+# pacman -Sy archlinux-keyring
 
-# pacstrap /mnt base
-# pacstrap /mnt base linux-firmware linux
-pacstrap /mnt base linux-firmware linux linux-headers base-devel
-# pacstrap /mnt linux-headers net-tools vim lvm2 nano iwd man-pages dialog dnsutils iputils wpa_supplicant dhcpcd
+echo -n ">>>> Instalando paquetes base\n"
+#pacstrap /mnt base
+#pacstrap /mnt base linux-firmware linux linux-headers base-devel
+pacstrap /mnt base linux-firmware linux linux-headers base-devel dhcpcd iputils dnsutils vim
+#pacstrap /mnt base linux-firmware linux efibootmgr grub-efi-x86_64 base-devel vim
+#pacstrap /mnt base linux-firmware linux efibootmgr grub-efi-x86_64 base-devel vim linux-headers net-tools dnsutils iputils dhcpcd
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
